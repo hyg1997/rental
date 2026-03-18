bcbe52f427d8b6f7d6102b8447e1fe410a325748
Hugo Yupanqui <hugo.yupanqui.g@gmail.com>
2026-03-18 01:22:16 -0500

    feat(05-02): wire homepage page.tsx to fetch from Sanity and pass section props
    - Import HOMEPAGE_CONTENT_QUERY from queries/homepage
    - Add homepageContent fetch to Promise.all (catch returns null)
    - Pass servicios, valores, metricas props to respective section components
    - Preserve existing HeroCarousel, FeaturedEquipment, ReclamacionesCta

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,6 +1,7 @@
 import { client } from '@/sanity/client'
 import { BANNERS_QUERY } from '@/sanity/queries/banners'
 import { FEATURED_EQUIPOS_QUERY } from '@/sanity/queries/home'
+import { HOMEPAGE_CONTENT_QUERY } from '@/sanity/queries/homepage'
 import { HeroCarousel } from '@/components/home/hero-carousel'
 import { ServicesSection } from '@/components/home/services-section'
 import { ValuesSection } from '@/components/home/values-section'
@@ -9,18 +10,19 @@ import { MetricsSection } from '@/components/home/metrics-counter'
 import { ReclamacionesCta } from '@/components/home/reclamaciones-cta'
 
 export default async function HomePage() {
-  const [banners, featuredEquipos] = await Promise.all([
+  const [banners, featuredEquipos, homepageContent] = await Promise.all([
     client.fetch(BANNERS_QUERY).catch(() => []),
     client.fetch(FEATURED_EQUIPOS_QUERY).catch(() => []),
+    client.fetch(HOMEPAGE_CONTENT_QUERY).catch(() => null),
   ])
 
   return (
     <>
       <HeroCarousel banners={banners} />
-      <ServicesSection />
-      <ValuesSection />
+      <ServicesSection servicios={homepageContent?.servicios ?? []} />
+      <ValuesSection valores={homepageContent?.valores ?? []} />
       <FeaturedEquipment equipos={featuredEquipos} />
-      <MetricsSection />
+      <MetricsSection metricas={homepageContent?.metricas ?? []} />
       <ReclamacionesCta />
     </>
   )
